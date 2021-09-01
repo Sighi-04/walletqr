ffa35954ed2547f3882052b273e9833ebe51caeb
emanuelfadda <emanuelfadda@MacBook-Air-di-emanuelfadda.local>
2021-09-01 10:05:14 +0200

    tolto il template di prova

--- src/App.vue
+++ src/App.vue
@@ -5,13 +5,8 @@
 </template>
 
 <script>
-<<<<<<< HEAD
-
-
-=======
 /*import HelloWorld from './components/HelloWorld.vue'*/
-import prova from './components/codiceLista.vue'
->>>>>>> ba334dd816f722777f7642c391f0aa48ff3b08cb
+//import prova from './components/codiceLista.vue'
 export default {
   name: 'App',
   components: {
