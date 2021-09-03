f5915f3cf2a0c2e054248379cd1809393325cf4d
emanuelfadda <emanuelfadda@MacBook-Air-di-emanuelfadda.local>
2021-09-03 12:28:45 +0200

    modificato ulteriormente il modal

--- src/components/visualizza.vue
+++ src/components/visualizza.vue
@@ -44,12 +44,12 @@
             <div class="modal-content">
                 <div class="modal-header">
                 <h5 class="modal-title">Sei sicuro?</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
                 <p>Sei sicuro di voler eliminare il codice salvato?</p>
                 </div>
                 <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Annulla</button>
                 <router-link to="/lista">
                     <button type="button" class="btn btn-danger btn-primary" data-bs-dismiss="modal">Elimina</button>
                 </router-link>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import router from './router/'
 import App from "./App.vue"
+
 Vue.config.productionTip = false
 new Vue({
   router,
