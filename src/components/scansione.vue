856680d1005731fb66ada5ec136573b8259a6725
Filippo Sighinolfi <filippo.sighinolfi.23@gmail.com>
2021-09-03 17:18:12 +0200

    Implementata scansione codice

--- src/components/scansione.vue
+++ src/components/scansione.vue
@@ -10,17 +10,31 @@
         <div class="my-4">        
             <p>Inquadra il codice, <br>
             assicurati che ci siano condizioni di luce buone</p>
-            <div id="reader" width="300px"></div>
+           <StreamBarcodeReader class="pt-5 pb-5-5 mb-3 mt-5"
+            @decode="onDecode">
+            </StreamBarcodeReader>
             <div class="container my-2">
                 <p>Scansione in corso...</p>
                 <div id="reader" width="600px"></div>
-                <router-link to="lista"><button type="button" class="btn btn-danger" style="width:70%;">Annulla</button></router-link>
+                <div class="mt-5"> <router-link to="lista"><button type="button" class="btn btn-danger" style="width:70%;">Annulla</button></router-link></div>
             </div>
         </div>
     </div>
 </template>
 
-
+<script>
+import { StreamBarcodeReader } from "vue-barcode-reader";
+    export default {
+        components: {
+            StreamBarcodeReader
+        },
+        methods: {
+            onDecode (result) {
+            console.log(result)
+        }
+}
+    }
+</script>
 
 <style scoped>
 
