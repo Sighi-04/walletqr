8f61ecd91cf8a48cf49728fdf7846667fd11be48
Filippo Sighinolfi <filippo.sighinolfi.23@gmail.com>
2021-09-07 10:58:52 +0200

    Aggiunta prop caller per mantenere il chiamante del componente, in modo da tornare alla schermata giusta

--- src/components/dettagli.vue
+++ src/components/dettagli.vue
@@ -1,8 +1,8 @@
-    <template>
+<template>
     <div>            
         <nav class="navbar navbar-light" style="background-color: #EA5D5D;">
             <div class="container-fluid">
-                <router-link to="/">
+                <router-link :to="{name: caller, params: {titolo: titolo, descrizione: descrizione, contenuto: contenuto, tipo: tipo, imgpreferiti: imgpreferiti}}">
                     <a class="navbar-brand">
                         <img src="assets/Icone/back_arrow.svg" alt="indietro" width="34" height="34">
                     </a>                
@@ -24,12 +24,29 @@
             <br>
         </div>
         <div class="container mt-4 pt-4 text-center">
-            <router-link to="/scansione"><img class="img-thumbnail shadow p-3 mb-5 bg-body rounded" src="assets/Icone/qrcode.svg" width="250px" height="250px"></router-link>
-            <p class="mt-5">Il codice è stato salvato, premi questa icona per scansionarlo nuovamente</p>
+            <router-link :to="{ name: 'scansione', params: { caller: 'dettagli'}}">
+                <button type="button" class="btn btn-info">Scansiona nuovamente</button>
+                </router-link>
             <div class="container mt-3 pt-3">
                 <div class="container m-1"><router-link to="/visualizza"><button type="button" style="width:70%" class="btn btn-success btn-lg ">Conferma</button></router-link></div>            
             </div>
         </div>
     </div>
 
-    </template>
+</template>
+<script>
+    export default {
+        props: {
+            imglogo: String,
+            titolo: String,
+            imgtipo: String,
+            imgpreferiti: String,
+            descrizione: String,
+            isBookmarked: Boolean,
+            tag: String,
+            tipo: String,
+            contenuto: String,
+            caller: String
+        }
+    }
+</script>
