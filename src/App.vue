886f028f37f71af5934fe5664d3e75ce18dac60d
Sighi-04 <filipposighinolfi@protonmail.com>
2021-09-09 16:36:33 +0200

    Rimosso script localstorage da app..vue

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,3 @@
     <router-view/>
   </div>
 </template>
-
-<script>
-  export default {
-  mounted() {
-      let scriptLocalStorage = document.createElement('script')
-      scriptLocalStorage.setAttribute('src', 'localStorage.js')
-      document.head.appendChild(scriptLocalStorage)
-    }
-}
-</script>
